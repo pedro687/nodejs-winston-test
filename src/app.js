2dfd1de2855859c7c68f7250aefdba5f40d04c29
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-05-24 01:15:38 -0300

    Criando logs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const logger = require('./utils/logger');
 const app = express();
 
+app.route('/').get((req, res) => {
+    logger.info("Usuario Autenticado");
+    res.send("Perfil do usuario");
+})
 
 app.listen(3000, () => {
     logger.info('Server Running')
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,18 @@
 const { createLogger, format, transports } = require('winston');
 
 module.exports = createLogger({
+    format: format.combine(
+        format.simple()
+    ),
     transports: [
+        new transports.File({
+            maxsize: 5120000,
+            maxFiles: 5,
+            filename: `${__dirname}/../logs/log-api.log`
+        }),
+
         new transports.Console({
-            level: 'debug',
-            format: format.combine(
-                format.simple()
-            )
+            level: 'debug'
         })
     ]
 })
